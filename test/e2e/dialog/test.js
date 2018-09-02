c182dcd9930a8fe38beda340dc72e5e45ffb5430
Qathom <tony9295@gmail.com>
2018-09-02 03:13:38 +0200

    Update e2e test

--- test/e2e/dialog/test.js
+++ test/e2e/dialog/test.js
@@ -1,57 +1,57 @@
 import { Selector, ClientFunction } from 'testcafe';
 
-fixture `Dialog`
+fixture `Modal`
   .page `./test.html`;
 
 
-test('Build a dialog without a title and message', async (t) => {
+test('Build a modal without a title and message', async (t) => {
   await ClientFunction(() => {
-    return phonon.dialog().show();
+    return phonon.modal().show();
   })();
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).ok()
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql('')
-    .expect(Selector('.dialog.show .dialog-body').innerText).eql('')
-    .click('.dialog.show .btn-primary')
+    .expect(Selector('.modal.show').exists).ok()
+    .expect(Selector('.modal.show .modal-title').innerText).eql('')
+    .expect(Selector('.modal.show .modal-body').innerText).eql('')
+    .click('.modal.show .btn-primary')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Build the same dialog with jQuery', async (t) => {
+test('Build the same modal with jQuery', async (t) => {
   await ClientFunction(() => {
-    return $().dialog().show();
+    return $().modal().show();
   })();
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).ok()
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql('')
-    .expect(Selector('.dialog.show .dialog-body').innerText).eql('')
-    .click('.dialog.show .btn-primary')
+    .expect(Selector('.modal.show').exists).ok()
+    .expect(Selector('.modal.show .modal-title').innerText).eql('')
+    .expect(Selector('.modal.show .modal-body').innerText).eql('')
+    .click('.modal.show .btn-primary')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Build a dialog with a title and a message', async (t) => {
+test('Build a modal with a title and a message', async (t) => {
   const title = 'My Title';
   const message = 'My message';
 
   await ClientFunction((title, message) => {
-    return phonon.dialog({ title, message }).show();
+    return phonon.modal({ title, message }).show();
   })(title, message);
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql(title)
-    .expect(Selector('.dialog.show .dialog-body p').innerText).eql(message)
-    .click('.dialog.show .btn-primary')
+    .expect(Selector('.modal.show .modal-title').innerText).eql(title)
+    .expect(Selector('.modal.show .modal-body p').innerText).eql(message)
+    .click('.modal.show .btn-primary')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Build a dialog with a title, a message and custom buttons', async (t) => {
+test('Build a modal with a title, a message and custom buttons', async (t) => {
   const title = 'My Title';
   const message = 'My message';
   const eventName = 'myEvent';
@@ -64,99 +64,99 @@ test('Build a dialog with a title, a message and custom buttons', async (t) => {
     },
   ];
 
-  // await t.eval(new Function(`phonon.dialog({title: "${title}", message: "${message}", buttons: ${JSON.stringify(buttons)}}).show();`))
+  // await t.eval(new Function(`phonon.modal({title: "${title}", message: "${message}", buttons: ${JSON.stringify(buttons)}}).show();`))
 
   await ClientFunction((title, message, buttons) => {
-    return phonon.dialog({ title, message, buttons }).show();
+    return phonon.modal({ title, message, buttons }).show();
   })(title, message, buttons);
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql(title)
-    .expect(Selector('.dialog.show .dialog-body p').innerText).eql(message)
-    .expect(Selector(`.dialog-footer [data-event="${eventName}"]`).innerText).eql('Custom button')
-    // close the dialog with the custom button
-    .click(`.dialog-footer [data-event="${eventName}"]`)
+    .expect(Selector('.modal.show .modal-title').innerText).eql(title)
+    .expect(Selector('.modal.show .modal-body p').innerText).eql(message)
+    .expect(Selector(`.modal-footer [data-event="${eventName}"]`).innerText).eql('Custom button')
+    // close the modal with the custom button
+    .click(`.modal-footer [data-event="${eventName}"]`)
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Show a dialog from a button', async (t) => {
-  const title = 'Dialog title';
-  const message = 'Dialog body text goes here.';
+test('Show a modal from a button', async (t) => {
+  const title = 'Modal title';
+  const message = 'Modal body text goes here.';
 
   await t
-    .click('[data-target="#exampleDialog"]')
+    .click('[data-target="#exampleModal"]')
     .wait(1000)
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql(title)
-    .expect(Selector('.dialog.show .dialog-body p').innerText).eql(message)
-    .click('.dialog.show .btn-primary')
+    .expect(Selector('.modal.show .modal-title').innerText).eql(title)
+    .expect(Selector('.modal.show .modal-body p').innerText).eql(message)
+    .click('.modal.show .btn-primary')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Close a cancelable dialog with Escape key', async (t) => {
+test('Close a cancelable modal with Escape key', async (t) => {
   await ClientFunction(() => {
-    return phonon.dialog().show();
+    return phonon.modal().show();
   })();
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).ok()
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql('')
-    .expect(Selector('.dialog.show .dialog-body').innerText).eql('')
+    .expect(Selector('.modal.show').exists).ok()
+    .expect(Selector('.modal.show .modal-title').innerText).eql('')
+    .expect(Selector('.modal.show .modal-body').innerText).eql('')
     .pressKey('esc')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Close a cancelable dialog by clicking on the backdrop', async (t) => {
+test('Close a cancelable modal by clicking on the backdrop', async (t) => {
   await ClientFunction(() => {
-    return phonon.dialog().show();
+    return phonon.modal().show();
   })();
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).ok()
-    .expect(Selector('.dialog.show .dialog-title').innerText).eql('')
-    .expect(Selector('.dialog.show .dialog-body').innerText).eql('')
-    .click('.dialog-backdrop', { offsetX: 5, offsetY: 5 })
+    .expect(Selector('.modal.show').exists).ok()
+    .expect(Selector('.modal.show .modal-title').innerText).eql('')
+    .expect(Selector('.modal.show .modal-body').innerText).eql('')
+    .click('.modal-backdrop', { offsetX: 5, offsetY: 5 })
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Test uncancelable dialog and then close with a button', async (t) => {
+test('Test uncancelable modal and then close with a button', async (t) => {
   await ClientFunction(() => {
-    return phonon.dialog({ cancelable: false }).show();
+    return phonon.modal({ cancelable: false }).show();
   })();
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).ok()
+    .expect(Selector('.modal.show').exists).ok()
     .pressKey('esc')
     .wait(1000)
-    .expect(Selector('.dialog.show').count).eql(1)
-    .click('.dialog-backdrop', { offsetX: 5, offsetY: 5 })
+    .expect(Selector('.modal.show').count).eql(1)
+    .click('.modal-backdrop', { offsetX: 5, offsetY: 5 })
     .wait(1000)
-    .expect(Selector('.dialog.show').count).eql(1)
-    .click('.dialog.show .btn-primary')
+    .expect(Selector('.modal.show').count).eql(1)
+    .click('.modal.show .btn-primary')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
 
-test('Test open 2 dialogs', async (t) => {
+test('Test open 2 modals', async (t) => {
   await ClientFunction(() => {
-    phonon.dialog().show();
-    phonon.dialog().show();
+    phonon.modal().show();
+    phonon.modal().show();
   })();
 
   await t
     .wait(1000)
-    .expect(Selector('.dialog.show').count).eql(2)
-    .click('.dialog-backdrop', { offsetX: 5, offsetY: 5 })
+    .expect(Selector('.modal.show').count).eql(2)
+    .click('.modal-backdrop', { offsetX: 5, offsetY: 5 })
     .wait(1000)
-    .expect(Selector('.dialog.show').count).eql(1)
-    .click('.dialog.show .btn-primary')
+    .expect(Selector('.modal.show').count).eql(1)
+    .click('.modal.show .btn-primary')
     .wait(1000)
-    .expect(Selector('.dialog.show').exists).notOk();
+    .expect(Selector('.modal.show').exists).notOk();
 });
